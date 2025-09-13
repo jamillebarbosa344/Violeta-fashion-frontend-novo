0ad3efc1c7d29c6e0b45348088948c1342994db1
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 18:06:18 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Página Inicial</h1>
-      <p>Bem-vindo à Violeta Fashion!</p>
+      <h1>Bem-vindo à Violeta Fashion</h1>
+      <p>Moda com estilo e elegância.</p>
     </Layout>
-  )
+  );
 }
