68d5938489b79f7af79594666ab62e524698d733
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 18:04:38 -0300

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -1,10 +1,10 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 export default function Cart() {
   return (
     <Layout>
       <h1>Carrinho</h1>
-      <p>Aqui estão os produtos que você adicionou.</p>
+      <p>Itens adicionados ao carrinho aparecem aqui.</p>
     </Layout>
-  )
+  );
 }
