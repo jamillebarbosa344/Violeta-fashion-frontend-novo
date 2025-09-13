c1ca5687ac032810623bd27e2b3b105c134b8e2f
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 13:27:51 -0300

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,31 @@
+import Navbar from "../components/Navbar";
+
+export default function Contact() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>Contato 📞</h1>
+        <p>Entre em contato com a Violeta Fashion pelos canais abaixo:</p>
+
+        <p>
+          📱 WhatsApp:{" "}
+          <a href="https://wa.me/5567993400590" target="_blank" rel="noreferrer">
+            Fale conosco
+          </a>
+        </p>
+
+        <p>
+          📸 Instagram:{" "}
+          <a href="https://instagram.com/seu_instagram" target="_blank" rel="noreferrer">
+            @seu_instagram
+          </a>
+        </p>
+
+        <p style={{ color: "gray" }}>
+          (Depois você pode trocar o número e o Instagram pelos seus reais 😉)
+        </p>
+      </div>
+    </div>
+  );
+}
