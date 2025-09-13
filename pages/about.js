58f677c47f638aebb0c384b9f06625a82ab00fd5
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 13:13:06 -0300

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Sobre a Violeta Fashion 🌸</h1>
+      <p>
+        Somos uma loja de moda feminina dedicada a trazer estilo, elegância e 
+        confiança para cada cliente.
+      </p>
+    </div>
+  );
+}
