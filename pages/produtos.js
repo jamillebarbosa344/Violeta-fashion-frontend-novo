365781bf2ea866d86ab905a24ad784b353a8a6c1
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 17:50:46 -0300

    Update produtos.js

--- pages/produtos.js
+++ pages/produtos.js
@@ -1,28 +1,10 @@
-const produtos = [
-  {
-    id: 1,
-    nome: "Conjunto Floral",
-    preco: "R$ 199,90",
-    image: "/produtos/IMG_3925.jpg",
-  },
-  {
-    id: 2,
-    nome: "Vestido Azul Elegante",
-    preco: "R$ 249,90",
-    image: "/produtos/IMG_3926.jpg",
-  },
-  {
-    id: 3,
-    nome: "Bolsa Branca Clássica",
-    preco: "R$ 159,90",
-    image: "/produtos/IMG_3927.jpg",
-  },
-  {
-    id: 4,
-    nome: "Sandália Nude Confort",
-    preco: "R$ 139,90",
-    image: "/produtos/IMG_3928.jpg",
-  },
-];
+import Layout from '../components/Layout'
 
-export default produtos;
+export default function Produtos() {
+  return (
+    <Layout>
+      <h1>Produtos</h1>
+      <p>Veja nossa coleção completa de produtos.</p>
+    </Layout>
+  )
+}
