278be2d3913453a0858974212baa93de502ba7e2
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 18:01:24 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
-import Header from './Header.js'
-import Footer from './Footer.js'
-import Navbar from './Navbar.js'
+import Header from './Header'
+import Footer from './Footer'
+import Navbar from './Navbar'
 
 export default function Layout({ children }) {
   return (
