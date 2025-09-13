546c53f7bda9ba5733511a36913723e1a2727a5e
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 14:22:08 -0300

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,41 +1,11 @@
-import Navbar from "../components/Navbar";
+import Layout from "./components/Layout";
 
 export default function Contact() {
   return (
-    <div>
-      <Navbar />
-      <div style={{ textAlign: "center", padding: "50px" }}>
-        <h1>Contato 📞</h1>
-        <p>Entre em contato com a Violeta Fashion pelos canais abaixo:</p>
-
-        <p>
-          📱 WhatsApp:{" "}
-          <a href="https://wa.me/5567993400590" target="_blank" rel="noreferrer">
-            Fale conosco
-          </a>
-        </p>
-
-        <p>
-          📸 Instagram:{" "}
-          <a href="https://instagram.com/seu_instagram" target="_blank" rel="noreferrer">
-            @seu_instagram
-          </a>
-        </p>
-
-        <p style={{ color: "gray" }}>
-          (Depois você pode trocar o número e o Instagram pelos seus reais 😉)
-        </p>
-      </div>
-    </div>
-  );
-}import Footer from "./components/Footer";
-
-export default function Home() {
-  return (
-    <div>
-      {/* conteúdo da Home */}
-      
-      <Footer />
-    </div>
+    <Layout>
+      <h2>Contato</h2>
+      <p>Email: contato@violetafashion.com</p>
+      <p>WhatsApp: (11) 99999-9999</p>
+    </Layout>
   );
 }
