b2a2284120a7ec3d8043bde862068999906f402d
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 17:41:56 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,12 @@
-import Header from "./Header";
-import Footer from "./Footer.js"; 
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Header />
-      <main style={{ minHeight: "80vh", padding: "20px" }}>
-        {children}
-      </main>
+    <>
+      <Navbar />
+      <main>{children}</main>
       <Footer />
-    </div>
+    </>
   );
 }
