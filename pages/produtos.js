cae6677adfbe3c8b91d315d1eaa22b31b8e462ec
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 18:24:20 -0300

    Update produtos.js

--- pages/produtos.js
+++ pages/produtos.js
@@ -1,10 +1,34 @@
-import Layout from '../components/Layout';
+const produtos = [
+  {
+    id: 1,
+    nome: "Produto 1",
+    preco: "R$ 99,90",
+    image: "/produtos/IMG_3925.jpg"
+  },
+  {
+    id: 2,
+    nome: "Produto 2",
+    preco: "R$ 129,90",
+    image: "/produtos/IMG_3926.jpg"
+  },
+  {
+    id: 3,
+    nome: "Produto 3",
+    preco: "R$ 149,90",
+    image: "/produtos/IMG_3927.jpg"
+  },
+  {
+    id: 4,
+    nome: "Produto 4",
+    preco: "R$ 79,90",
+    image: "/produtos/IMG_3928.jpg"
+  },
+  {
+    id: 5,
+    nome: "Produto 5",
+    preco: "R$ 199,90",
+    image: "/produtos/IMG_3929.jpg"
+  }
+];
 
-export default function Produtos() {
-  return (
-    <Layout>
-      <h1>Produtos</h1>
-      <p>Aqui você encontra nossos produtos disponíveis.</p>
-    </Layout>
-  );
-}
+export default produtos;
