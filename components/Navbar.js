9c170e3d81471b2b7060a5ad03a6250b2c308e54
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-13 17:40:03 -0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,14 @@
-import Link from "next/link";
-import "./Navbar.css"; // vamos criar esse css já já
+import Link from 'next/link';
+import './Navbar.css';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Violeta Fashion</div>
+      <h1 className="logo">Violeta Fashion</h1>
       <ul className="nav-links">
         <li><Link href="/">Início</Link></li>
-        <li><Link href="/about">Sobre</Link></li>
         <li><Link href="/produtos">Produtos</Link></li>
+        <li><Link href="/about">Sobre</Link></li>
         <li><Link href="/contact">Contato</Link></li>
         <li><Link href="/cart">Carrinho</Link></li>
       </ul>
