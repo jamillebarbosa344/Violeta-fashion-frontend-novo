61e5c73a73ece7283b4af3f4697e090c8de03059
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-14 12:53:34 -0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,10 @@ import Footer from '../components/Footer';
 
 export default function About() {
   return (
-    <Layout>
+    <>
       <h1>Sobre Nós</h1>
       <p>Aqui você pode falar sobre a loja Violeta Fashion.</p>
-    </Layout>
+      <Footer />
+    </>
   );
 }
