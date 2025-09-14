3c10f6faa1652b48a93bcebe63d4ff197084827b
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-14 14:52:21 -0300

    Update  footer.js

--- components/ footer.js
+++ components/ footer.js
@@ -1,8 +1,8 @@
-// components/Footer.js
+// components/footer.js
 export default function Footer() {
   return (
-    <footer style={{ textAlign: "center", padding: "20px", background: "#f5f5f5" }}>
-      <p>© {new Date().getFullYear()} Violeta Fashion. Todos os direitos reservados.</p>
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} VIOLETA FASHION. TODOS OS DIREITOS RESERVADOS.</p>
     </footer>
   );
 }
