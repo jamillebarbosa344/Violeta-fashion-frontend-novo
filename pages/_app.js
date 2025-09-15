dd32fc06e374d45810a8030ea73d70182cf248e8
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 18:04:26 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      {/* Navbar no topo */}
+      <Navbar />
+
+      {/* Header logo abaixo */}
+      <Header />
+
+      {/* Conteúdo da página */}
+      <main>
+        <Component {...pageProps} />
+      </main>
+
+      {/* Footer no final */}
       <Footer />
     </>
   )
