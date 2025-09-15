1de04d3e4231d5154365a58f268fa1b8515a396f
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 18:22:16 -0300

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,15 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 export default function Contact() {
   return (
-    <div style={{ textAlign: "center", padding: "40px" }}>
-      <h1>Contato</h1>
-      <p>Email: contato@violetafashion.com</p>
-      <p>WhatsApp: (11) 99999-9999</p>
-    </div>
+    <>
+      <Header />
+      <main style={{ padding: "20px" }}>
+        <h1>Contato</h1>
+        <p>Entre em contato conosco pelo e-mail: contato@violetafashion.com</p>
+      </main>
+      <Footer />
+    </>
   )
 }
