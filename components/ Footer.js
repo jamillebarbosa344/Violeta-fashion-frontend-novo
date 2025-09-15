e8b2a92154fd927a7dc2587772855a6a11cf4e46
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 18:52:43 -0300

    Delete components/ Footer.js

--- components/ Footer.js
+++
@@ -1,9 +0,0 @@
-import './Footer.css';
-
-export default function Footer() {
-  return (
-    <footer className="Footer">
-      <p>© {new Date().getFullYear()} Violeta Fashion. Todos os direitos reservados.</p>
-    </footer>
-  );
-}
