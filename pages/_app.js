8ba50836ec47ce834b6919db11e7de2420f1830a
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 18:07:51 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,18 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { useRouter } from 'next/router'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
   return (
     <>
       {/* Navbar no topo */}
       <Navbar />
 
-      {/* Header logo abaixo */}
-      <Header />
+      {/* Header só aparece na Home */}
+      {router.pathname === '/' && <Header />}
 
       {/* Conteúdo da página */}
       <main>
