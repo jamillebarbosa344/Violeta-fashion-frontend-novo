04bd3dd5d1d9cd49967f278604b1b2c801630446
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 18:03:19 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
 import '../styles/globals.css'
+import Footer from '../components/Footer'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
       <Footer />
     </>
   )
