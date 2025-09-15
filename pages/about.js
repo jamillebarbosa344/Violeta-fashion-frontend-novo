4f1ba0e88f41b0374feebca1f2f5e795826a13b1
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 10:55:59 -0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-import Layout from './components/Layout';
-
 export default function About() {
   return (
-    <Layout>
+    <div style={{ textAlign: "center", padding: "40px" }}>
       <h1>Sobre Nós</h1>
-      <p>Aqui você pode falar sobre a loja Violeta Fashion.</p>
-    </Layout>
-  );
+      <p>
+        A Violeta Fashion é uma loja dedicada a trazer moda de qualidade,
+        estilo e conforto para você.
+      </p>
+    </div>
+  )
 }
