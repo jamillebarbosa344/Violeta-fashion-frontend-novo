1647060e3e6788e5040dd02423249f9fddd1a8e2
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 18:23:12 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", padding: "40px" }}>
-      <h1>Bem-vindo à Violeta Fashion</h1>
-      <p>As melhores roupas e acessórios para você</p>
-    </div>
+    <>
+      <Header />
+      <main style={{ padding: "20px" }}>
+        <h1>Bem-vindo à Violeta Fashion</h1>
+        <p>Moda com estilo e sofisticação para todas as ocasiões.</p>
+      </main>
+      <Footer />
+    </>
   )
 }
