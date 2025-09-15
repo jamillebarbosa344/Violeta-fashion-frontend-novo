2300f5eea1a1300f4ca3578c659f5c9fc6d7024d
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 18:20:45 -0300

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -1,8 +1,15 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 export default function Cart() {
   return (
-    <div style={{ textAlign: "center", padding: "40px" }}>
-      <h1>Carrinho</h1>
-      <p>Seu carrinho está vazio.</p>
-    </div>
+    <>
+      <Header />
+      <main style={{ padding: "20px" }}>
+        <h1>Carrinho</h1>
+        <p>Seu carrinho está vazio no momento.</p>
+      </main>
+      <Footer />
+    </>
   )
 }
