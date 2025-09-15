218e18d9d5791bd5eeaea62fcfe4cf44a6c8c9f9
jamillebarbosa344 <jamillebarbosa344@gmail.com>
2025-09-15 09:25:06 -0300

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,10 +1,10 @@
 import Layout from '../components/Layout';
 
-export default function Contact() {
+export default function Checkout() {
   return (
     <Layout>
-      <h1>Contato</h1>
-      <p>Aqui você pode colocar informações de contato.</p>
+      <h1>Finalizar Compra</h1>
+      <p>Aqui você pode colocar informações de pagamento e envio.</p>
     </Layout>
   );
 }
